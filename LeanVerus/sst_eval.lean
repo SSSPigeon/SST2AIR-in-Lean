9e42f67da4d4adc8775597e10856aaed9d7d396e
rongshuge <shugerong@gmail.com>
2026-01-06 01:28:07 -0500

    var_env as a list

--- LeanVerus/sst_eval.lean
+++ LeanVerus/sst_eval.lean
@@ -1,9 +1,20 @@
 import LeanVerus.My_sst
+import LeanVerus.Typing
 
 open VerusLean Std
 
 abbrev var_env := List Exp
 
-abbrev typ_env := HashMap Typ Typ
+abbrev typ_env := Typ → Typ
 
-abbrev func_env := HashMap Typ Exp
+abbrev func_env := Typ → Exp
+
+inductive Eval: var_env → func_env → Exp → Exp → Prop where
+  | const: ∀ (v: var_env) (f: func_env) (c: Const),
+    WsTm v.length (.Const c) →
+    Eval v f (.Const c) (.Const c)
+
+  | var: ∀ (v: var_env) (f: func_env) (i: Nat) (e: Exp),
+    WsTm v.length (.Var i) →
+    v[i]? = e →
+    Eval v f (.Var i) e
