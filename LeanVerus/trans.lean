2df81ae306028d425727a3a75d53e64182eb6e92
rongshuge <shugerong@gmail.com>
2026-02-10 22:11:05 -0500

    before renaming VerusLean

--- LeanVerus/trans.lean
+++ LeanVerus/trans.lean
@@ -2,5 +2,10 @@ import LeanVerus.Typ
 import LeanVerus.Typing
 import LeanVerus.Exp
 import LeanVerus.Domain
+import LeanVerus.Ast
 
-open VerusLean typing
+open VerusLean typing airast
+
+variable (tenv : typ_env)  (dom_aux : ClosedTyp → Type)
+
+def trans (e : Exp):
